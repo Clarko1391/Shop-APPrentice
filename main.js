bd5fa08159430e54f865e6e7610a5d3499c442e9
Clarko1391 <62443535+Clarko1391@users.noreply.github.com>
2020-08-11 13:02:53 -0230

    this is a mess

---
+++ main.js
@@ -0,0 +1,26 @@
+// Selectors & Event Listeners
+const toolSelector = document.getElementById('tool').addEventListener('click', function(){console.log("ya")});
+const conversionSelector = document.getElementById('conversionDD').addEventListener('click', function(){console.log('yaya')});
+const toolName = document.getElementById('toolName');
+const conversion = document.getElementById('conversion');
+const decMMUp = document.getElementById('decMMUp');
+const decMM = document.getElementById('decMM');
+const decMMDown = document.getElementById('decMMDown');
+const decInchUp = document.getElementById('decInchUp');
+const decInch = document.getElementById('decInch');
+const decInchDown = document.getElementById('decInchDown');
+const fracInchUp = document.getElementById('fracInchUp');
+const fracInch = document.getElementById('fracInch');
+const fracInchDown = document.getElementById('fracInchDown');
+const userEntry = document.getElementById('userEntry').addEventListener('click', function (){userInput()});
+document.getElementById('refresh').addEventListener('click', console.log(distance));
+
+// Functions
+function userInput (){
+    let distance = userEntry.value;
+    console.log('this');
+}
+
+// function convert(conversionSelector, userEntry) {
+
+// }
