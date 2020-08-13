a5683dcd5802c1ddd7860685a5c306bb27ac95dd
Clarko1391 <62443535+Clarko1391@users.noreply.github.com>
2020-08-12 21:30:03 -0230

    updates all around
    - added dropdown menus and buttons for dropdown icons to CSS and HTML
    
    - started in on js for dropdowns

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
 // Selectors & Event Listeners
-const toolSelector = document.getElementById('tool').addEventListener('click', function(){console.log("ya")});
-const conversionSelector = document.getElementById('conversionDD').addEventListener('click', function(){console.log('yaya')});
-const toolName = document.getElementById('toolName');
-const conversion = document.getElementById('conversion');
-const decMMUp = document.getElementById('decMMUp');
-const decMM = document.getElementById('decMM');
-const decMMDown = document.getElementById('decMMDown');
-const decInchUp = document.getElementById('decInchUp');
-const decInch = document.getElementById('decInch');
-const decInchDown = document.getElementById('decInchDown');
-const fracInchUp = document.getElementById('fracInchUp');
-const fracInch = document.getElementById('fracInch');
-const fracInchDown = document.getElementById('fracInchDown');
-const userEntry = document.getElementById('userEntry').addEventListener('input', function (){userInput()});
-document.getElementById('refresh').addEventListener('click', console.log(distance));
+document.getElementById('userEntry').addEventListener('input', userInput);
+let measurement = '';
+let conversionSelector = document.getElementById('conversion').innerText
+let decInch = document.getElementById('decInch');
+let decInchUp = document.getElementById('decInchUp');
+let decInchDown = document.getElementById('decInchDown');
+let decMM = document.getElementById('decMM');
+let decMMUp = document.getElementById('decMMUp');
+let decMMDown = document.getElementById('decMMDown');
+const toolDropDown = document.getElementById('toolDropDown');
+document.getElementById('toolDD').addEventListener('click', function() {toolMenu()});
+
+
 
 // Functions
 function userInput (){
-    let distance = userEntry.value;
-    console.log('this');
+    userString = userEntry.value;
+    measurement = parseInt(userString);
+    convert(measurement);
+    console.log(typeof(measurement));
 }
 
-// function convert(conversionSelector, userEntry) {
+function toolMenu (){
+    toolDropDown.classList.toggle('dropDownActive');
+    console.log('change');
+}
 
-// }
+function convert(measurement) {
+    if (conversionSelector === "mm to inches") {
+        decMM.innerHTML = measurement;
+        oneUp = (measurement + 1);
+        decMMUp.innerHTML = oneUp
+        oneDown = (measurement - 1);
+        decMMDown.innerHTML = oneDown
+        answer = measurement * 0.0393701;
+        answerUp = oneUp * 0.0393701;
+        answerDown = oneDown * 0.0393701;
+        answer = +answer.toFixed(2);
+        answerUp = +answerUp.toFixed(2);
+        answerDown = +answerDown.toFixed(2);
+        decInch.innerHTML = answer;
+        decInchUp.innerHTML = answerUp;
+        decInchDown.innerHTML = answerDown;
+
+    } else if (conversionSelector === "inches to mm - Decimal") {
+        decInch.innerHTML = measurement;
+        answer = measurement/0.0393701;
+        decMM.innerHTML = answer
+
+    } else if (conversionSelector === "inches to mm - Fractional") {
+
+    }
+}
+
